9e5e33f3e41552df9f13cb6d125f0243ceb79edf
Ahmed Ashraf <ahmedashraf@pop-os.localdomain>
2022-01-07 20:42:53 +0200

    Create table

--- dbms.sh
+++ dbms.sh
@@ -35,4 +35,118 @@ function ReadTableNameFromUSer
     Tablename=$(whiptail --inputbox "Enter Table name" 8 39 3>&1 1>&2 2>&3)
 }
 
+
+
+
+function Create_table
+{
+    ReadTableNameFromUSer
+    seperator="|"
+    raw_seperator="\n"
+    pkey=""
+    declare columnsNumber
+    declare -A metaData_data
+    if [ -f $Tablename ]
+    then
+        zenity --error --title="Error!" --text="$Tablename already exist!" --no-wrap
+    else
+        
+        while ! [[ $columnsNumber =~ ^[0-9]+$ ]]
+        do
+           columnsNumber=$(whiptail --inputbox "Enter Number of Columns" 8 39 3>&1 1>&2 2>&3)
+        done
+
+        metaData_def="Column Name"$seperator"Type"$seperator"Primary Key"
+        
+        showNumber=1
+        for (( counter = 0; counter < $columnsNumber; counter++ ));
+        do
+            _counter=0
+            #echo -e "Name of Column Number "$showNumber": \c"
+            #read columnName
+            columnName=$(whiptail --inputbox "Enter Name of Column Number $showNumber" 8 39 3>&1 1>&2 2>&3)
+            ((showNumber++))
+            data[$counter]=$columnName
+            metaData_data[$counter,$_counter]=$columnName # metaData_data[0,0] = id
+            ((_counter++))
+
+            input=$(whiptail --title "Type of Column $columnName" --fb --menu "Choose data type" 15 50 6 \
+            "1" "integer" \
+            "2" "string"  3>&1 1>&2 2>&3)
+            case $input in
+                    1 ) 
+                        columnType="int"
+                    ;;
+                    2 ) 
+                        columnType="str"
+                    ;;
+            esac
+
+            metaData_data[$counter,$_counter]=$columnType # metaData_data[0,1] = type(int or str)
+            ((_counter++))
+             #zenity --error --title="Error!" --text="$pKey here!" --no-wrap
+            if [[ $pKey == "" ]]; 
+            then
+                input=$(whiptail --title "Would you like to set as Primary Key ?" --fb --menu "Set a PK" 15 50 6 \
+                "1" "yes" \
+                "2" "no"  3>&1 1>&2 2>&3)
+                case $input in
+                     1 ) pKey="PK"; metaData_data[$counter,$_counter]="$pKey";
+                        ;;
+                     2 ) metaData_data[$counter,$_counter]="";
+                        ;;
+                esac
+            else
+                metaData_data[$counter,$_counter]="";
+            fi
+
+        done
+        
+        if [[ $pKey != "" ]]
+        then 
+                # Print MetaData
+                touch .$Tablename
+                echo -e $metaData_def >> .$Tablename
+                for (( counter = 0; counter < $columnsNumber; counter++ ));
+                do  
+                    for (( _counter = 0; _counter < 3; _counter++ ));
+                    do
+                        if [ $_counter == 2 ]
+                        then
+                            echo -n -e "${metaData_data[$counter,$_counter]}" >> .$Tablename
+                        else
+                            echo -n -e "${metaData_data[$counter,$_counter]}""$seperator" >> .$Tablename  
+                        fi 
+                    done  
+                    echo -n -e "$raw_seperator" >> .$Tablename  
+                done
+
+                # Print Actual Data
+                touch $Tablename
+                ((flag=$columnsNumber-1))
+                for (( counter = 0; counter < $columnsNumber; counter++ )); 
+                do
+                    if [[ $counter == $flag ]]
+                    then
+                        echo -n "${data[$counter]}"  >> $Tablename
+                        echo -n -e "$raw_seperator" >> $Tablename
+                    else
+                        echo -n "${data[$counter]}""$seperator" >> $Tablename
+                    fi
+                done
+        fi
+        # Check The Process
+        if [[ $? == 0 && $pKey != "" ]]
+        then
+            zenity --info --title="Completed!" --text="Table Created Successfully :)" --no-wrap
+        else
+            zenity --error --title="Error" --text="NOT Successful Creation of Table $Tablename ! \n You must set a column as a primary key" --no-wrap
+        fi
+    fi   
+    unset pKey   
+}
+
+
+
+
 ]
